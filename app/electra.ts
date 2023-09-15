0b25043405c9bfdfd7a6aed21d83e349a0611338
pintoinfant <pintoinfant5650@gmail.com>
2023-09-16 03:16:08 +0530

    update electra thingy

--- app/electra.ts
+++ app/electra.ts
@@ -13,18 +13,18 @@ const app = express();
 const port = 3000;
 
 // Initialize your Ethereum provider (e.g., Infura)
-// const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
-const provider = new ethers.providers.JsonRpcProvider('https://polygon-mumbai-bor.publicnode.com');
+const provider = new ethers.providers.JsonRpcProvider('https://althea.zone:8545');
+// const provider = new ethers.providers.JsonRpcProvider('https://polygon-mumbai-bor.publicnode.com');
 
 
 const client = new MongoClient(process.env.MONGO_URI as string);
 const db = client.db('electra')
 
 
-const electraTokenAddress = "0x1C6686e806873786439E3C36A8740B99A9028eB4"
-const governorContractAddress = "0xeD5c0474f76f5F68Fa1c4BE284e2Dc2A263fB990"
-const StationRegistryAddress = "0x98e0E4d8ba9A783A7Dd725C9270bDD857523DeCa"
-const VehicleLedgerAddress = "0x38770155e8598214F46144C154d62eC21474ee36"
+const electraTokenAddress = "0x211B1fCEf17F8B5a02Df4bE392A07603A6C83469"
+const governorContractAddress = "0x155b38a3D15F21497bc050Db6093F7bc539f1458"
+const StationRegistryAddress = "0xEBC1bAb4403E8F8f170781F7d4817039031eE675"
+const VehicleLedgerAddress = "0x51255104075Ff8c8B9c1Dece4d44b922FA7A27C7"
 
 // Connect to the contract
 const tokenContract = new ethers.Contract(electraTokenAddress, electraTokenABI.abi, provider);
@@ -38,25 +38,25 @@ const VehicleLedgerContract = new ethers.Contract(VehicleLedgerAddress, VehicleL
 tokenContract.on('*', async (event) => {
   // console.log('Event emitted:');
   console.log(event); // Log the event data
-  await db.collection('electra-events').insertOne({ ...event, chainId: 80001 })
+  await db.collection('electra-events').insertOne({ ...event, chainId: 417834 })
 });
 
 governorContract.on('*', async (event) => {
   // console.log('Event emitted:');
   console.log(event); // Log the event data
-  await db.collection('tcr-registry').insertOne({ ...event, chainId: 80001 })
+  await db.collection('electra-events').insertOne({ ...event, chainId: 417834 })
 });
 
 StationRegistryContract.on('*', async (event) => {
   // console.log('Event emitted:');
   console.log(event); // Log the event data
-  await db.collection('tcr-registry').insertOne({ ...event, chainId: 80001 })
+  await db.collection('electra-events').insertOne({ ...event, chainId: 417834 })
 });
 
 VehicleLedgerContract.on('*', async (event) => {
   // console.log('Event emitted:');
   console.log(event); // Log the event data
-  await db.collection('tcr-registry').insertOne({ ...event, chainId: 80001 })
+  await db.collection('electra-events').insertOne({ ...event, chainId: 417834 })
 });
 
 // Start the Express.js server
